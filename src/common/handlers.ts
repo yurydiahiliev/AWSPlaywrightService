3613a7017ca5c3d2d870e5252a2ef6ebe8511230
yuriidiahiliev <yu.dyagilev@gmail.com>
2024-04-14 12:20:43 +0300

    added base structure

---
+++ src/common/handlers.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export async function handleCreateSpotFleetRequest(request: Request, response: Response) {
+    
+}
+
+export async function handleRunPlaywrightTests(request: Request, response: Response) {
+    
+}
+
+export async function handleDeleteSpotFleet(request: Request<{spotFleetId: string}>, response: Response) {
+    
+}
---
+++ src/common/routes.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from "express";
+import { handleCreateSpotFleetRequest, handleDeleteSpotFleet, handleRunPlaywrightTests } from "./handlers";
+
+const router = Router();
+
+router.post('/createSpotFleet', async (request: Request, response: Response) => {
+    await handleCreateSpotFleetRequest(request, response);
+})
+
+router.post('/runPlaywright', async (req: Request, res: Response) => {
+    await handleRunPlaywrightTests(req, res);
+});
+
+router.delete('/deleteSpotFleet/:spotFleetId', async (req: Request<{ spotFleetId: string }>, res: Response) => {
+    await handleDeleteSpotFleet(req, res);
+});
